37e4549321386e3d6220ac07faa51395f5446387
Freya Olsson <47249151+OlssonF@users.noreply.github.com>
2022-09-06 09:39:13 -0400

    Update run_forecasts.R

--- run_forecasts.R
+++ run_forecasts.R
@@ -1,6 +1,6 @@
 
 # Script to run forecasts
-source('./models/ARIMA_model.R')
+source('./Models/ARIMA_model.R')
 message('ARIMA model submitted')
-source('./models/TSLM_lags.R')
+source('./Models/TSLM_lags.R')
 message('TSLM_lagged model submitted')
