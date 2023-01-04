7d7473fa6cdc51c0f98b4454ea81aaa5c40842b9
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2023-01-04 18:26:58 -0500

    script to access scores from bucket

---
+++ get_scores.R
@@ -0,0 +1,20 @@
+Sys.unsetenv("AWS_ACCESS_KEY_ID")
+Sys.unsetenv("AWS_SECRET_ACCESS_KEY")
+Sys.unsetenv("AWS_DEFAULT_REGION")
+Sys.unsetenv("AWS_S3_ENDPOINT")
+Sys.setenv(AWS_EC2_METADATA_DISABLED="TRUE")
+library(arrow)
+library(tidyverse)
+
+s3 <- s3_bucket(paste0("neon4cast-scores/parquet/aquatics"), endpoint_override= "data.ecoforecast.org")
+
+your_model_id <- 'persistenceRW'
+
+scores <- open_dataset(s3) |>
+  filter(model_id == your_model_id) |> 
+  collect()
+
+scores |> 
+  na.omit(observation) |> 
+  group_by(model_id, reference_datetime, site_id, datetime) |> 
+  summarize(crps_score = mean(crps, na.rm = TRUE)) 
